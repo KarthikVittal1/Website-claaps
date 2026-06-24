9da46da39aea64084949c00fc38a12c8d2ab28a3
Srikar Balemarthy <Srikar.Balemarthy@claaps.com>
2026-06-24 05:45:03 +0000

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   turbopack: {
     root: path.join(__dirname),
   },
+
   images: {
     remotePatterns: [
       {
